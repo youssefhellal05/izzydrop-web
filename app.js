75d17d088d3903137cf6bb5ae3468ff9f0cc3729
youssefhellal05 <youssefhellal05@gmail.com>
2026-09-24 05:35:58 +0300

    Use latest storefront widget build

--- app.js
+++ app.js
@@ -318,7 +318,7 @@
   }
 
   function makeEmbedCode(token){
-    return `<div data-izzydrop-token="${token}"></div>\n<script src="https://youssefhellal05.github.io/izzydrop-web/izzydrop-widget.js?v=20260924-finish1" async><\/script>`;
+    return `<div data-izzydrop-token="${token}"></div>\n<script src="https://youssefhellal05.github.io/izzydrop-web/izzydrop-widget.js?v=20260924-finish4" async><\/script>`;
   }
 
   async function openWebSetup(productId){
